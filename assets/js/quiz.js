54db9da79bf42a60c56e0e21f37e562504988069
Blakedoghyian <xblakedx@gmail.com>
2021-11-14 14:44:43 -0600

    added styling to page and buttons, started js var

---
+++ assets/js/quiz.js
@@ -0,0 +1,10 @@
+
+// variables to reference DOM elements
+var questionsEl = document.getElementById("questions");
+var timerEl = document.getElementById("time");
+var choicesEl = document.getElementById("choices");
+var submitBtn =  document.getElementById("submit");
+var startBtn = document.getElementById("start");
+var intialsEl = document.getElementById("initials");
+
+// start quiz function
